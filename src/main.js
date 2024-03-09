f3d7218687af775420508a69b0ca420d60bf6cb4
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-10 01:34:48 +0500

    fix: get_time() only for pages/main.html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
+const get_time = () =>
+{
+	const get_info = (x) =>
+	{
+		const year = x.getFullYear();
+		const month = (x.getMonth() + 1).toString().padStart(2, '0');
+		const day = x.getDate().toString().padStart(2, '0');
+	  const hours = x.getHours().toString().padStart(2, '0');
+	  const minutes = x.getMinutes().toString().padStart(2, '0');
+
+	  return `${day}-${month}-${year} / ${hours}:${minutes}`;
+	};
+	
+	const current_time = new Date();
+  const local_offset = current_time.getTimezoneOffset() * 60000;
+  const target_offset = 5 * 3600000;
+  const date = new Date(current_time.getTime() + target_offset + local_offset);
+
+	document.getElementById("local_time").innerHTML = get_info(date);
+};
+
+
 const menu = (x) =>
 {
+	if (x == 1) get_time(); 
+
 	const div = document.createElement('ul');
 	const items = [
 		{ name : "About me", link : "../pages/main.html", },
@@ -30,27 +54,3 @@ const menu = (x) =>
 
 	document.getElementById("menu-block").append(div);
 }
-
-const test_time = () =>
-{
-	const get_info = (x) =>
-	{
-		const year = x.getFullYear();
-		const month = (x.getMonth() + 1).toString().padStart(2, '0');
-		const day = x.getDate().toString().padStart(2, '0');
-	  const hours = x.getHours().toString().padStart(2, '0');
-	  const minutes = x.getMinutes().toString().padStart(2, '0');
-
-	  return `${day}-${month}-${year} / ${hours}:${minutes}`;
-	};
-	
-	const current_time = new Date();
-  const local_offset = current_time.getTimezoneOffset() * 60000;
-  const target_offset = 5 * 3600000;
-  const date = new Date(current_time.getTime() + target_offset + local_offset);
-
-	document.getElementById("local_time").innerHTML = get_info(date);
-};
-
-test_time();
-
