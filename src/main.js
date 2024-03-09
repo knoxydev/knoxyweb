7581720c5031ab74ef14bc9d3eb4925b3b7a9e6c
knoxydev <nkgithubnarkuziev108@gmail.com>
2024-03-10 01:27:23 +0500

    update: minor changes

--- src/main.js
+++ src/main.js
@@ -31,3 +31,26 @@ const menu = (x) =>
 	document.getElementById("menu-block").append(div);
 }
 
+const test_time = () =>
+{
+	const get_info = (x) =>
+	{
+		const year = x.getFullYear();
+		const month = (x.getMonth() + 1).toString().padStart(2, '0');
+		const day = x.getDate().toString().padStart(2, '0');
+	  const hours = x.getHours().toString().padStart(2, '0');
+	  const minutes = x.getMinutes().toString().padStart(2, '0');
+
+	  return `${day}-${month}-${year} / ${hours}:${minutes}`;
+	};
+	
+	const current_time = new Date();
+  const local_offset = current_time.getTimezoneOffset() * 60000;
+  const target_offset = 5 * 3600000;
+  const date = new Date(current_time.getTime() + target_offset + local_offset);
+
+	document.getElementById("local_time").innerHTML = get_info(date);
+};
+
+test_time();
+
